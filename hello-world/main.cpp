47921311f0bc65d438e8fbcc2124aa8380e9e14a
Richard Derryberry <richardtderryberry@gmail.com>
2022-11-29 00:58:34 -0600

    Initial Commit

---
+++ hello-world/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  hello-world
+//
+//  Created by Richard Derryberry on 11/29/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
