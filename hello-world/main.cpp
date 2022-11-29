4234171370080fd7382bf93a32e368df08256bd1
Richard Derryberry <richardtderryberry@gmail.com>
2022-11-29 01:02:46 -0600

    [hello-world] Write "Hello world" program.

--- hello-world/main.cpp
+++ hello-world/main.cpp
@@ -7,8 +7,7 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    std::cout << "Hello, world!\n";
     return 0;
 }
